6eddf9ad9de5c0cae5100ebaab2e4c53d3bb6620
hawaros <46148032+hawaros@users.noreply.github.com>
2019-07-16 00:31:34 +0200

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,5 @@ from mxdGeesi import app
 
 
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.secret_key = '1235674ABCDEF'
+    app.run(debug = True)
